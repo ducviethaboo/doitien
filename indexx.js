552e465b1cf065c1bb78f01834cf40df3f5c3eb2
tibeohaboo <macos@MACs-MacBook-Pro.local>
2020-09-10 17:06:39 +0700

    add baitap

---
+++ indexx.js
@@ -0,0 +1,8 @@
+
+function getSum(){
+    let t1 = document.getElementById("asd").value;
+    let t2 = document.getElementById("abc").value;
+    let t3 = document.getElementById("bcd").value;
+    let tiendoi = t1*t3/t2
+    document.getElementById("ketqua").innerText="Result :" + tiendoi
+}
